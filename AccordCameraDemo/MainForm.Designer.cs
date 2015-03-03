d6d2ca681db5032bd1de9de13f0f867b9cab6766
hoangitk <hoangitk@gmail.com>
2015-03-03 14:33:19 +0700

    Init

---
+++ AccordCameraDemo/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace AccordCameraDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox picCamera;
+        private System.Windows.Forms.PictureBox picPortrait;
+        private System.Windows.Forms.Button btnCapture;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCamera = new System.Windows.Forms.PictureBox();
+            this.picPortrait = new System.Windows.Forms.PictureBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCamera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPortrait)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCamera
+            // 
+            this.picCamera.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picCamera.Location = new System.Drawing.Point(20, 12);
+            this.picCamera.Name = "picCamera";
+            this.picCamera.Size = new System.Drawing.Size(400, 300);
+            this.picCamera.TabIndex = 0;
+            this.picCamera.TabStop = false;
+            // 
+            // picPortrait
+            // 
+            this.picPortrait.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picPortrait.Location = new System.Drawing.Point(440, 12);
+            this.picPortrait.Name = "picPortrait";
+            this.picPortrait.Size = new System.Drawing.Size(240, 300);
+            this.picPortrait.TabIndex = 1;
+            this.picPortrait.TabStop = false;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(159, 323);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(75, 23);
+            this.btnCapture.TabIndex = 2;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(520, 323);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 369);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.picPortrait);
+            this.Controls.Add(this.picCamera);
+            this.Name = "MainForm";
+            this.Text = "Demo: Capture Image from Camera";
+            ((System.ComponentModel.ISupportInitialize)(this.picCamera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPortrait)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ AccordCameraDemo/MainForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accord.Extensions.Imaging;
+using Accord.Extensions.Vision;
+
+namespace AccordCameraDemo
+{
+    public partial class MainForm : Form
+    {
+    	ImageStreamReader _reader;
+    	
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            this.picCamera.SizeMode = PictureBoxSizeMode.Zoom;
+            this.picPortrait.SizeMode = PictureBoxSizeMode.Zoom;
+            
+            this.picCamera.Paint += picCamera_Paint;
+            
+            _reader = new CameraCapture(cameraIdx: 0);
+            _reader.Open();
+            
+            this.FormClosing += MainForm_FormClosing;
+            Application.Idle += Application_Idle;
+            this.btnCapture.Click += btnCapture_Click;
+            this.btnSave.Click += btnSave_Click;
+        }
+
+		void picCamera_Paint(object sender, PaintEventArgs e)
+		{
+			PictureBox picBox = sender as PictureBox;
+			
+			var pW = picBox.Width / 5 * 3;
+			var pH = picBox.Height;
+			
+			Rectangle portraitBorder = 
+				new Rectangle((picBox.Width - pW) / 2, 0, pW, pH);
+			
+			using (var pen = new Pen(Color.Red, 2))
+			{
+				e.Graphics.DrawRectangle(pen, portraitBorder);
+			}
+		}
+		
+        Image<Bgr, byte> _frame = null;
+		void Application_Idle(object sender, EventArgs e)
+		{
+			_frame = _reader.ReadAs<Bgr, byte>();
+			
+			if(_frame == null)
+			{
+				Application.Idle -= Application_Idle;
+				return;
+			}
+			
+			var imgCaputre = _frame.ToBitmap();
+			this.picCamera.Image = imgCaputre;
+			GC.Collect();
+		}
+			
+		void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if(_reader != null)
+				_reader.Dispose();
+		}
+
+		void btnCapture_Click(object sender, EventArgs e)
+		{
+			if(_frame != null)
+			{
+				var imgCapture = _frame.ToBitmap();
+				
+				var pW = imgCapture.Width / 5 * 3;
+				var pH = imgCapture.Height;
+			
+				Rectangle portraitBorder = 
+					new Rectangle((imgCapture.Width - pW) / 2, 0, pW, pH);
+			
+				var imgPortrait = imgCapture.Clone(portraitBorder, imgCapture.PixelFormat);
+				
+				this.picPortrait.Image = imgPortrait;
+			}
+		}
+
+		void btnSave_Click(object sender, EventArgs e)
+		{
+			using (var dlg = new SaveFileDialog()) 
+			{
+				dlg.FileName = "portrait";
+				dlg.DefaultExt = ".jpg";
+				dlg.Filter = "JPeg Image|*.jpg";
+				
+				if(dlg.ShowDialog() == DialogResult.OK)
+				{
+					var fileName = dlg.FileName;
+					
+					this.picPortrait.Image.Save(fileName, ImageFormat.Jpeg);
+				}
+			}
+		}
+    }
+}
